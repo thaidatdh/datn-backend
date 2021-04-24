854bdd9fada6f627e8b769c23ba533beb586b607
Dat Ho <thaidatdh@gmail.com>
2021-04-24 09:54:41 +0700

    fix error start server

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,9 +2,7 @@ let router = require("express").Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 let authController = require("../controllers/auth.controller");
 router.route("/signin").post(authController.signin_staff);
-router
-  .route("/change-password")
-  .post(authMiddleware, authController.change_password);
+router.route("/change-password").post(authController.change_password);
 router.route("/refresh-token").post(authController.refresh_token);
 router.route("/patient/signin").post(authController.signin_patient);
 module.exports = router;
