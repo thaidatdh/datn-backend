a927a5c377c6e0e5d4dbedd8fb1133fa44c4b59d
Dat Ho <thaidatdh@gmail.com>
2021-05-28 07:28:07 +0700

    update payment route

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -6,10 +6,13 @@ router
   .get(TransactionController.index)
   .post(TransactionController.add);
 router
-  .route("/:treatment_id")
+  .route("/:transaction_id")
   .get(TransactionController.detail)
   .patch(TransactionController.update);
 router
   .route("/patient/:patient_id")
   .get(TransactionController.patient_transaction);
+router
+  .route("/new/:patient_id")
+  .get(TransactionController.get_new_transaction_info);
 module.exports = router;
